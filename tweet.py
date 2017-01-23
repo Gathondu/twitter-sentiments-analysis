9c8373a3f768e70f2c21df63c0b524a2bb38020b
Denis Gathondu <thundoss@gmail.com>
2017-01-24 00:56:08 +0300

    Logic To Detect Username With @ Symbol
    - Added logic in the tweet.py that ensures if the user uses an @ char it is removed
    
    - Also added a try/except clause to see that the username provided is legit

--- tweet.py
+++ tweet.py
@@ -2,7 +2,10 @@
 
 import json
 import tweepy
+import re
 import tweet_secrets as secrets
+
+from user_prompts import *
 
 
 class Tweet(object):
@@ -12,3 +15,22 @@ class Tweet(object):
     auth.set_access_token(secrets.ACCESS_TOKEN, secrets.ACCESS_SECRET)
 
     api = tweepy.API(auth)  # initialize a tweepy API class with credentials
+
+    def __init__(self):
+        self.userName = None
+        self.period = None
+        self.terminate = None
+
+    def prompt(self):
+        try:
+            self.userName = input(user_prompts.user_name)
+            # check if user used @ and remove it
+            if re.match(r'^@', self.userName):
+                chars = self.userName.split()
+                chars.remove('@')
+                self.userName = ''.join(chars)
+            user_data = tweepy.Cursor(api.user_timeline,
+                                      id=self.userName).item(1)
+        except TweepError as t:
+            print(t['message'])
+            print(user_prompts.invalid_user)
