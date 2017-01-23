b19ba033669f3bbed0c7f581c4e5a91f96c43fee
Denis Gathondu <thundoss@gmail.com>
2017-01-23 21:30:01 +0300

    Obtained status update for a user but hardcoded

--- tweet.py
+++ tweet.py
@@ -9,11 +9,14 @@ auth = tweepy.OAuthHandler(secrets.KEY, secrets.SECRET)
 auth.set_access_token(secrets.ACCESS_TOKEN, secrets.ACCESS_SECRET)
 
 api = tweepy.API(auth)
-user = api.me()
+user_tweets = tweepy.Cursor(api.user_timeline, id="Nimzee_Nimo").items()
 
-print('Name: ' + user.name)
-print('Location: ' + user.location)
-print('Friends: ' + str(user.friends_count))
+for tweet in user_tweets:
+    print(tweet.text)
+
+# print('Name: ' + user_name)
+# print('Location: ' + user_location)
+# print('Friends: ' + str(user_friends_count))
 
 public_tweets = api.home_timeline()
 # for tweet in public_tweets:
